28f1e2cc9a49c85442b2bc10f0beae3496009494
Dohyeon_Parrk <parkyj3213@naver.com>
2025-01-31 17:54:38 +0900

    feat: User -> Entity 생성

---
+++ src/main/java/com/program/typingpractice/domain/user/User.java
@@ -0,0 +1,47 @@
+package com.program.typingpractice.domain.user;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+	@Column(name = "role")
+	private Set<String> roles;
+
+	@Builder
+	public User(String username, String password, Set<String> roles) {
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+}
