bb0d8a009db9900e23b7a963bc533cf3ccf74760
Dohyeon_Parrk <parkyj3213@naver.com>
2025-02-05 20:11:20 +0900

    fix: User 엔티티 컬럼 추가, 수정으로 dto 수정

--- src/main/java/com/program/typingpractice/dto/user/RegisterRequestDto.java
+++ src/main/java/com/program/typingpractice/dto/user/RegisterRequestDto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequestDto {
+	private String email;
 	private String username;
 	private String password;
 }
