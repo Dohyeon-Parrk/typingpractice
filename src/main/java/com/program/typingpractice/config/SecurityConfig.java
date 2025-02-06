47412ba830d7b7509cb59560d083b09e0533abff
Dohyeon_Parrk <parkyj3213@naver.com>
2025-02-06 12:53:47 +0900

    refactor: Security formLogin -> 비활성화

--- src/main/java/com/program/typingpractice/config/SecurityConfig.java
+++ src/main/java/com/program/typingpractice/config/SecurityConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.program.typingpractice.repository.user.UserRepository;
@@ -31,11 +34,7 @@ public class SecurityConfig {
 				.requestMatchers("/api/auth/**").permitAll()
 				.anyRequest().authenticated()
 			)
-			.formLogin(form -> form
-				.loginProcessingUrl("/api/auth/login")
-				.defaultSuccessUrl("/", true)
-				.permitAll()
-			)
+			.formLogin(AbstractHttpConfigurer::disable)
 			.logout(logout -> logout
 				.logoutUrl("/api/auth/logout")
 				.logoutSuccessUrl("/")
@@ -52,11 +51,6 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService(UserRepository userRepository){
-		return new CustomUserDetailsService(userRepository);
 	}
 
 	@Bean
