644ae83db8dfdf6d58be9ab6e00fc5158b314fb1
Do-hyeon Park <parkyj3213@naver.com>
2025-02-08 17:35:27 +0900

    feat: ErrorCode -> User 상태 코드 추가2

--- src/main/java/com/program/typingpractice/global/ErrorCode.java
+++ src/main/java/com/program/typingpractice/global/ErrorCode.java
@@ -14,15 +14,16 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
 
     // 회원 관련 예외
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 이메일 입니다."),
+    USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 닉네임 입니다."),
     INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "올바른 이메일 형식을 입력해 주세요."),
-    USERNAME_REQUIRED(HttpStatus.BAD_REQUEST, "유저네임은 필수 입력 사항입니다."),
-    PASSWORD_REQUIRED(HttpStatus.BAD_REQUEST, "비밀번호는 필수 입력 사항입니다."),
+    USERNAME_REQUIRED(HttpStatus.BAD_REQUEST, "유저네임은 필수 입력 사항 입니다."),
+    PASSWORD_REQUIRED(HttpStatus.BAD_REQUEST, "비밀번호는 필수 입력 사항 입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     WRONG_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
 
     // 인증 관련 예외
-    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "인증이 필요 합니다."),
     FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "권한이 없습니다.");
 
     private final HttpStatus status;
