f90d7b6773a470017d5d0ef8281118de90e1f14d
Dohyeon_Parrk <parkyj3213@naver.com>
2025-02-13 18:30:11 +0900

    refactor: Role enum 클래스 추가로 인한 리팩토링

--- src/main/java/com/program/typingpractice/domain/user/User.java
+++ src/main/java/com/program/typingpractice/domain/user/User.java
@@ -1,22 +1,13 @@
 package com.program.typingpractice.domain.user;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-
-import java.util.Set;
-
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Entity
@@ -39,14 +30,21 @@ public class User {
 
 	@ElementCollection(fetch = FetchType.EAGER)
 	@CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+	@Enumerated(EnumType.STRING)
 	@Column(name = "role")
-	private Set<String> roles;
+	private Set<Role> roles;
 
 	@Builder
-	public User(String email, String username, String password, Set<String> roles) {
+	public User(String email, String username, String password, Set<Role> roles) {
 		this.email = email;
 		this.username = username;
 		this.password = password;
 		this.roles = roles;
+	}
+
+	public Set<String> getRoleNames() {
+		return roles.stream()
+				.map(Enum::name)
+				.collect(Collectors.toSet());
 	}
 }
